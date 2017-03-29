ea3275d7053c8744fb4afdf9074535a01c25a699
Lykin <lecjian@163.com>
2017-03-29 20:26:32 +0800

    Add group class

--- ChatRobot/Chat.py
+++ ChatRobot/Chat.py
@@ -1,8 +1,11 @@
 from Login import Login
 from Contacts import Contacts
 from Message import Message
+from Group import Group
 
 if __name__ == '__main__':
     login = Login()
     contacts = Contacts()
+    group = Group()
     message = Message(contacts)
+    
---
+++ ChatRobot/Group.py
@@ -0,0 +1,136 @@
+import json
+import os
+import time
+import Data
+import Tools
+
+class Group:
+    def __init__(self):
+        self.group_members = {}
+        self.get_groups_members()
+    
+    def get_groups_members(self):
+        url = Data.url_base + '/webwxbatchgetcontact?type=ex&r=%s&pass_ticket=%s' % (int(time.time()), Data.pass_ticket)
+        params = {
+            'BaseRequest': Data.base_request,
+            "Count": len(Data.group_list),
+            "List": [{"UserName": group['UserName'], "EncryChatRoomId": ""} for group in Data.group_list]
+        }
+        result = Data.session.post(url, data = json.dumps(params))
+        result.encoding = Data.encoding
+        dic = json.loads(result.text)
+        for group in dic['ContactList']:
+            gid = group['UserName']
+            members = group['MemberList']
+            self.group_members[gid] = members
+        if Data.DEBUG:
+            Tools.write_file(json.dumps(self.group_members), os.path.join(Data.TEMP_DIR, 'group_members.json'), 'wb')
+
+    def get_uid_by_name(self, group_name):
+        for group in Data.group_list:
+            if group['NickName'] == group_name:
+                return group['UserName']
+        return None
+
+    def get_name_by_uid(self, uid):
+        for group in Data.group_list:
+            if group['UserName'] == uid:
+                return group['NickName']
+        return None
+
+    def add_friend_to_group(self, uid, group_name):
+        '''Add friends to group chat'''
+        group_id = self.get_uid_by_name(group_name)
+        if group_id is None: return False
+
+        for group_user in self.group_members[group_id]:
+            if group_user['UserName'] == uid:
+                return True #user in group
+
+        group_num = len(self.group_members[group_id])
+        if group_num <= 100:
+            url = Data.url_base + '/webwxupdatechatroom?fun=addmember&pass_ticket=%s' % Data.pass_ticket
+            params ={
+                "AddMemberList": uid,
+                "ChatRoomName": group_id,
+                "BaseRequest": Data.base_request
+            }
+        else:
+            url = Data.url_base + '/webwxupdatechatroom?fun=invitemember'
+            params ={
+                "InviteMemberList": uid,
+                "ChatRoomName": group_id,
+                "BaseRequest": Data.base_request
+            }
+        headers = {'content-type': 'application/json; charset=UTF-8'}
+        data = json.dumps(params, ensure_ascii = False).encode('utf8')
+        try:
+            result = Data.session.post(url, data = data, headers = headers)
+        except (ConnectionError, ReadTimeout):
+            return False
+        dic = result.json()
+        return dic['BaseResponse']['Ret'] == 0
+
+    def invite_friend_to_group(self,uid,group_name):
+        group_id = self.get_uid_by_name(group_name)
+        if group_id is None: return False
+
+        for group_user in self.group_members[group_id]:
+            if group_user['UserName'] == uid:
+                return True #user in group
+
+        url = Data.url_base + '/webwxupdatechatroom?fun=invitemember&pass_ticket=%s' % Data.pass_ticket
+        params = {
+            "InviteMemberList": uid,
+            "ChatRoomName": group_id,
+            "BaseRequest": Data.base_request
+        }
+        headers = {'content-type': 'application/json; charset=UTF-8'}
+        data = json.dumps(params, ensure_ascii = False).encode('utf8')
+        try:
+            result = Data.session.post(url, data = data, headers = headers)
+        except (ConnectionError, ReadTimeout):
+            return False
+        dic = result.json()
+        return dic['BaseResponse']['Ret'] == 0
+
+    def delete_user_from_group(self, user_name, group_id):
+        user_id = None
+        for user in self.group_members[group_id]:
+            if user['NickName'] == user_name:
+                user_id = user['UserName']
+        if user_id == None:
+            return False
+        url = Data.url_base + '/webwxupdatechatroom?fun=delmember&pass_ticket=%s' % Data.pass_ticket
+        params ={
+            "DelMemberList": user_id,
+            "ChatRoomName": group_id,
+            "BaseRequest": Data.base_request
+        }
+        headers = {'content-type': 'application/json; charset=UTF-8'}
+        data = json.dumps(params, ensure_ascii = False).encode('utf8')
+        try:
+            result = Data.session.post(url, data = data, headers = headers)
+        except (ConnectionError, ReadTimeout):
+            return False
+        dic = result.json()
+        return dic['BaseResponse']['Ret'] == 0
+
+    def set_group_name(self, group_id, group_name):
+        url = Data.url_base + '/webwxupdatechatroom?fun=modtopic&pass_ticket=%s' % Data.pass_ticket
+        params ={
+            "NewTopic": group_name,
+            "ChatRoomName": group_id,
+            "BaseRequest": Data.base_request
+        }
+        headers = {'content-type': 'application/json; charset=UTF-8'}
+        data = json.dumps(params, ensure_ascii = False).encode('utf8')
+        try:
+            result = Data.session.post(url, data = data, headers = headers)
+        except (ConnectionError, ReadTimeout):
+            return False
+        dic = result.json()
+        return dic['BaseResponse']['Ret'] == 0
+
+
+
